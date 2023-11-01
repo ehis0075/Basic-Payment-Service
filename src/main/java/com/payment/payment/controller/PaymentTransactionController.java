94f2eb1dbb615df0222e5f5b9c0706399172a60b
ehis0075 <ehisjude420@gmail.com>
2023-11-01 13:49:24 +0100

    fixed bugs

--- src/main/java/com/payment/payment/controller/PaymentTransactionController.java
+++ src/main/java/com/payment/payment/controller/PaymentTransactionController.java
@@ -23,7 +23,6 @@ public class PaymentTransactionController {
 
     private final GeneralService generalService;
 
-//    @PreAuthorize("hasAuthority('MAKE_PAYMENT')")
     @PostMapping("/pay")
     public Response payment(@RequestBody PaymentTransactionPayload requestDTO) {
 
@@ -31,7 +30,6 @@ public class PaymentTransactionController {
         return generalService.prepareResponse(ResponseCodeAndMessage.SUCCESSFUL_0, data);
     }
 
-    //    @PreAuthorize("hasAuthority('VIEW_PAYMENT')")
     @GetMapping("/{referenceNumber}")
     public Response getOneTransaction(@PathVariable String referenceNumber) {
 
@@ -39,7 +37,6 @@ public class PaymentTransactionController {
         return generalService.prepareResponse(ResponseCodeAndMessage.SUCCESSFUL_0, data);
     }
 
-    //    @PreAuthorize("hasAuthority('VIEW_PAYMENT')")
     @GetMapping("/get/{merchantId}")
     public Response getAllTransaction(@PathVariable String merchantId) {
 
